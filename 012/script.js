adfadd393c4ba7a86b581d47bee7673a84a57e4a
João Tosetti <tosetti.joao@gmail.com>
2022-02-10 21:40:30 -0300

    resolução exercício 12

---
+++ 012/script.js
@@ -0,0 +1,9 @@
+let altura
+let peso_ideal
+
+console.log("Informe sua altura para saber seu peso ideal")
+altura = 1.89
+
+peso_ideal = (72.7 * altura) - 58
+
+console.log("Seu peso ideal seria :", peso_ideal)
