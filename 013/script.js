8f5e060aa8992939625d10ba1df17fbb9e1960ab
João Tosetti <tosetti.joao@gmail.com>
2022-02-10 21:51:25 -0300

    resolução do exercício 13

---
+++ 013/script.js
@@ -0,0 +1,21 @@
+let genero
+let altura
+let peso_ideal
+
+console.log("Qual seu gênero?")
+genero = "homem"
+
+if (genero = "homem") {
+    console.log("Digite sua altura :")
+    altura = 1.90
+
+    peso_ideal = (72.7 * altura) - 58
+    console.log("Seu peso ideal seria :", peso_ideal)
+}
+else {
+    console.log("Digite sua altura :")
+    altura = 1.20
+
+    peso_ideal = (62.1 * altura) - 44.7
+    console.log("Seu peso ideal seria :", peso_ideal)
+}
