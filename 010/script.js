91b3a72ef871449d99344079261c7a0617bf2d00
João Tosetti <tosetti.joao@gmail.com>
2022-02-10 21:34:32 -0300

    resolução do exercício 10

---
+++ 010/script.js
@@ -0,0 +1,8 @@
+let temp_c
+let temp_f
+
+console.log("Informe a temperatura em graus Celsius para transformá-la em Fahrenheit")
+temp_c = 231
+
+temp_f = (temp_c * 9/5) + 32
+console.log(temp_c, "em Fahrenheit é :", temp_f)
