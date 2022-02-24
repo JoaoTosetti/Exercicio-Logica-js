39e3392e9e434486cd1a3336298e6f61e2876d1f
João Tosetti <tosetti.joao@gmail.com>
2022-02-24 20:32:36 -0300

    adaptação do exercício 17 com funções

--- 017/script.js
+++ 017/script.js
@@ -1,14 +1,15 @@
-let n1, n2
-n1 = -584
-n2 = 2012
-n3 = 6541
+function maiorNumero(n1, n2, n3) {
+    if (n1 > n2 && n1 > n3) {
+        console.log("O maior número é", n1)
+    } else if (n2 > n1 && n2 > n3) {
+        console.log("O maior número é", n2)
+    } else if (n3 > n1 && n3 > n2) {
+        console.log("O maior número é", n3)
+    }
+}
+maiorNumero(645,186354,236)
 
-if (n1 > n2 && n1 > n3) {
-    console.log("O maior número é", n1)
-} 
-if (n2 > n1 && n2 > n3) {
-    console.log("O maior número é", n2)
+function soma(n1=0, n2=0) {
+    console.log(n1 + n2)
 }
-if (n3 > n1 && n3 > n2) {
-    console.log("O maior número é", n3)
-}
+soma(2,5)
