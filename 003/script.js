c0084213f568aa1ca8f359187536e7b4725784d2
João Tosetti <tosetti.joao@gmail.com>
2022-03-03 22:01:37 -0300

    continuação

--- 003/script.js
+++ 003/script.js
@@ -1,5 +1,5 @@
-function somar(n1=0,n2=0) {
-console.log("Digite um número")
-console.log("Digite outro número")
-console.log("A soma dos dois números é : ", n1 + n2)
+function somar(n1=0,n2=0,) {
+    n1 = parseFloat(n1)
+    n2 = parseFloat(n2)
+    document.querySelector("#resultado").innerHTML = "A soma dos dois números é : " + n1+n2
 }
