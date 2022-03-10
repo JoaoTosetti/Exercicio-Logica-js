1c67fdcf90e49db5b6f8bf620c29f885a08191cd
João Tosetti <tosetti.joao@gmail.com>
2022-03-10 20:39:34 -0300

    finalização do exercício 4

--- 004/script.js
+++ 004/script.js
@@ -1,10 +1,21 @@
-function media(n1=0, n2=0, n3=0, n4=0) {
-console.log("Escreva suas 4 notas bimestrais para saber a média")
-console.log("Escreva a primeira nota:")
-console.log("Escreva a segunda nota:")
-console.log("Escreva a terceira nota:")
-console.log("Escreva a quarta nota:")
-console.log("Sua média é : ", (n1 + n2 + n3 + n4)/4)
+function calcular_media() {
+    let n1 = document.querySelector("#nota1").value
+    let n2 = document.querySelector("#nota2").value
+    let n3 = document.querySelector("#nota3").value
+    let n4 = document.querySelector("#nota4").value
+    let media
+    media = parseFloat(media)
+    n1 = parseFloat(n1)
+    n2 = parseFloat(n2)
+    n3 = parseFloat(n3)
+    n4 = parseFloat(n4)
+    media = (n1 + n2 + n3 + n4)/4
+    
+    /*console.log("Escreva a primeira nota:")
+    console.log("Escreva a segunda nota:")
+    console.log("Escreva a terceira nota:")
+    console.log("Escreva a quarta nota:") */
+    document.querySelector("#resultado").innerHTML = "Sua média é: " + media
+
 }
 
-media(6,6,4,5)
