a28d3a7743cd2bdb4a611e2d288a9f9d520783b6
João Tosetti <tosetti.joao@gmail.com>
2022-03-10 20:55:14 -0300

    resolução do exercício 5

--- 005/script.js
+++ 005/script.js
@@ -1,6 +1,12 @@
-function centimetros(metro=0) {
-    console.log("Digite um valor em metros para saber em centímetros:")
-    console.log("O número em centímetros é ", metro*100, "cm")
-}
+function centimetros() {
+    let metro = document.querySelector("#metro").value
+    let centimetro
 
-centimetros(87)
+    centimetro = parseFloat(centimetro)
+    metro = parseFloat(metro)
+    centimetro = metro * 100
+    
+    document.querySelector("#resultado").innerHTML = "O valor em centímetro é: " + centimetro + "cm"
+    /* console.log("Digite um valor em metros para saber em centímetros:")
+    console.log("O número em centímetros é ", metro*100, "cm")*/
+}
