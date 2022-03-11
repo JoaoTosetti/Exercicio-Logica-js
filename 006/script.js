7dc01c872a7f360d76d9b60e0acb74d289bf9895
João Tosetti <tosetti.joao@gmail.com>
2022-03-10 21:20:49 -0300

    finalização do exercício 007

--- 006/script.js
+++ 006/script.js
@@ -1,6 +1,14 @@
-function area(raio=0) {
-    console.log("Informe o raio do círculo:")
-    console.log("A área desse círculo é", (raio * raio) * 3.14, "cm²")
+function calcular_area() {
+    let raio = document.querySelector("#raio").value
+    let area
+    raio = parseFloat(raio)
+    area = parseFloat(area)
+    area = (raio * raio) * 3.14
+
+    document.querySelector("#resultado").innerHTML = "A área desse circulo é " + area + "cm²"
+
+
+    /*console.log("Informe o raio do círculo:")
+    console.log("A área desse círculo é", (raio * raio) * 3.14, "cm²")*/
 }
 
-area(65)
--- 007/script.js
+++ 007/script.js
@@ -1,8 +1,18 @@
-function areaDobro(b=0, h=0) {
+function area_dobro() {
     let area
-    console.log("Para descobrir a área do quadrado nos informe o valor da sua base: ")
+    let area2 
+    let b = document.querySelector("#base").value
+    let h = document.querySelector("#altura").value
+    area = parseFloat(area)
+    area2 = parseFloat(area2)
+    b = parseFloat(b)
+    h = parseFloat(h)
+    area = b * h
+    area2 = area * 2
+
+    document.querySelector("#resultado").innerHTML = "A área desse quadrado é " + area + "cm²" + " e o dobro da área é " + area2 + "cm²"
+    /*console.log("Para descobrir a área do quadrado nos informe o valor da sua base: ")
     console.log("Agora precisamos do valor da sua altura: ")
     area = b * h
-    console.log("O dobro da área desse quadrado é ", area*2, "cm²")
+    console.log("O dobro da área desse quadrado é ", area*2, "cm²")*/
 }
-areaDobro(5,9)
